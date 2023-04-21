f971bc7a75eebc35eca307e4652c42a7181f3d1e
kennywindis <kennyphilip100@gmail.com>
2023-04-20 21:15:35 -0700

    pt

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,14 +7,14 @@ def pascal_triangle(n):
     """returns pascal's triangle of n"""
 
     if n <= 0
+
 return []
-for i in range(n):
-for k in range(n-i+1):
+    k = [[1]]
+    for i in range(n - 1):
+        t = [0] + k[-1] + [0]
+        row = []
+        for d in range(len(k[-1]) + 1):
+            row.append(t[d] + t[d + 1])
+        out.append(row)
+    return k
 
-return (end='')
-
-for k in range(i+1):
-    
-return(factorial(i)//factorial(k)*factorial(i-k)), end=" ")
-
-return()
