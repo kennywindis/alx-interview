2a421f091652ea10492cbb563eb796f35c0b732c
kennywindis <kennyphilip100@gmail.com>
2023-04-20 21:09:04 -0700

    pt

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,8 +8,13 @@ def pascal_triangle(n):
 
     if n <= 0
 return []
-for i in range(n+1):
-for k in range(n-i):
-return (' ', end='')
+for i in range(n):
+for k in range(n-i+1):
 
-return(n)
+return (end='')
+
+for k in range(i+1):
+    
+return(factorial(i)//factorial(k)*factorial(i-k)), end=" ")
+
+return()
