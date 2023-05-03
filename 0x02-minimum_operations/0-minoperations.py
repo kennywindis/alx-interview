662ed3b0f6a5ce0b3b0982d2fb9b94c9e6da377c
kennywindis <kennyphilip100@gmail.com>
2023-05-03 07:01:07 -0700

    mo

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,7 +7,7 @@ def minOperations(n):
     mink = 2
     while n > 1:
         while n % mink == 0:
-            nk += mink
+            nOpe += mink
             n /= mink
-        mink += 1
+        
     return nk
