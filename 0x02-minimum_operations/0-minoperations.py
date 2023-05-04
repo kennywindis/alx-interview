c2ed37ac17ad1b46016e8c5158dea37a6df48e42
kennywindis <kennyphilip100@gmail.com>
2023-05-04 02:42:53 -0700

    mo

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+"""
+Minimum operations
+"""
 
-"""minimum operation"""
 
 def minOperations(n):
-    opr = {}
-
-    def s(n):
-        if n<0:
-            return float("k")
-
-        if n==0:
-            return 0
-        if n in memo:
-            return memo[n]
-        if n%2++0
-        memo[n] = 1 + s(n//2)
-    else:
-        memo[n] = 1 + s(n-1)
-        return memo[n]
-    return s(n)
+    """
+    :param n:
+    :return:
+    """
+    if n <= 1:
+        return 0
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
