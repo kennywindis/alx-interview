66d308d127a3bc5429cea3784c1587232c8a42b2
kennywindis <kennyphilip100@gmail.com>
2023-05-18 05:53:00 -0700

    8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """UTF-8 Validation Module
 """
-
-
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding."""
-    k_bytes = 0
-
-    for num in data:
-        byte = num & 0xff
-        if k_bytes == 0:
-            mask1 = 1 << 7
-            mask2 = 1 << 6
-            while mask1 & byte:
-                n_bytes += 1
-                mask1 = mask1 >> 1
-                mask2 = mask2 >> 1
-            if k_bytes == 0:
+    """ UTF-8 Validation """
+    bytesLong = 0
+    binaryMoveLeft7 = 1 << 7
+    binaryMoveLeft6 = 1 << 6
+    for byte in data:
+        binaryMove = 1 << 7
+        if bytesLong == 0:
+            while byte & binaryMove:
+                bytesLong += 1
+                binaryMove = binaryMove >> 1
+            if bytesLong == 0:
                 continue
-            if k_bytes == 1 or k_bytes > 4:
+            if bytesLong == 1 or bytesLong > 4:
                 return False
         else:
-            mask1 = 1 << 7
-            mask2 = 1 << 6
-            if not (byte & mask1 and not (byte & mask2)):
+            if not (byte & binaryMoveLeft7 and not (byte & binaryMoveLeft6)):
                 return False
-        k_bytes -= 1
-    return k_bytes == 0
+        bytesLong -= 1
+    if bytesLong == 0:
+        return True
+    return False
