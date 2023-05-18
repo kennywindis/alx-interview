630ac72eb6d4948fd3a50bcf50e6fcd12a0bef71
kennywindis <kennyphilip100@gmail.com>
2023-05-18 05:50:00 -0700

    8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-"""
-Method that determines if a given data set represents a valid UTF-8 encoding.
-* Return: True if data is a valid UTF-8 encoding, else return False
-* A character in UTF-8 can be 1 to 4 bytes long
-* The data set can contain multiple characters
-* The data will be represented by a list of integers
-* Each integer represents 1 byte of data, therefore you only need
-* to handle the 8 least significant bits of each integer
+"""UTF-8 Validation Module
 """
 
 
 def validUTF8(data):
-    """ UTF-8 Validation """
-    bytesLong = 0
-    binaryMoveLeft7 = 1 << 7
-    binaryMoveLeft6 = 1 << 6
-    for byte in data:
-        binaryMove = 1 << 7
-        if bytesLong == 0:
-            while byte & binaryMove:
-                bytesLong += 1
-                binaryMove = binaryMove >> 1
-            if bytesLong == 0:
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+    k_bytes = 0
+
+    for num in data:
+        byte = num & 0xff
+        if k_bytes == 0:
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            while mask1 & byte:
+                n_bytes += 1
+                mask1 = mask1 >> 1
+                mask2 = mask2 >> 1
+            if k_bytes == 0:
                 continue
-            if bytesLong == 1 or bytesLong > 4:
+            if k_bytes == 1 or k_bytes > 4:
                 return False
         else:
-            if not (byte & binaryMoveLeft7 and not (byte & binaryMoveLeft6)):
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            if not (byte & mask1 and not (byte & mask2)):
                 return False
-        bytesLong -= 1
-    if bytesLong == 0:
-        return True
-    return False
-
+        k_bytes -= 1
+    return k_bytes == 0
