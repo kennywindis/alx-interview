12c677dba3ff0ca76e5157956a681c61d2383bd8
kennywindis <kennyphilip100@gmail.com>
2023-06-16 04:00:44 -0700

    nq

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,131 +1,89 @@
 #!/usr/bin/python3
-"""
-   Description: The N queens puzzle is the challenge of placing N non-attacking
-                queens on an N×N chessboard. Write a program that solves the N
-                queens problem.
-   Usage: nqueens N:
-          If the user called the program with the wrong number of arguments,
-          print Usage: nqueens N, followed by a new line, and exit with the
-          status 1
-   where N must be an integer greater or equal to 4
-"""
-
-
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    sys.exit(1)
+else:
+    N = int(sys.argv[1])
 
-def print_board(board):
-    """ print_board
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+
+def printBoard(board):
+    """Print formated board
     Args:
-        board - list of list with length sys.argv[1]
+        board (list): list of list of 0 or 1
     """
-    new_list = []
-    for i, row in enumerate(board):
-        value = []
-        for j, col in enumerate(row):
-            if col == 1:
-                value.append(i)
-                value.append(j)
-        new_list.append(value)
-
-    print(new_list)
+    board_vect = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                board_vect.append([i, j])
+                break
+    print(board_vect)
 
 
-def isSafe(board, row, col, number):
-    """ isSafe
+def valid_pos(board, row=0, col=0):
+    """Check valid positions in col
     Args:
-        board - list of list with length sys.argv[1]
-        row - row to check if is safe doing a movement in this position
-        col - col to check if is safe doing a movement in this position
-        number: size of the board
-    Return: True of False
+        row (int): row number of matrix
+        col (int): colum number of matrix
     """
-
-    # Check this row in the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
         if board[i][j] == 1:
             return False
-
-    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while j >= 0 and i < N:
         if board[i][j] == 1:
             return False
-
+        i += 1
+        j -= 1
     return True
 
 
-def solveNQUtil(board, col, number):
-    """ Auxiliar method to find the posibilities of answer
+def Solver(board, col=0):
+    """Vefify the options
     Args:
-        board - Board to resolve
-        col - Number of col
-        number - size of the board
-    Returns:
-        All the posibilites to solve the problem
+        col (int): colum number of matrix
     """
-
-    if (col == number):
-        print_board(board)
+    if col >= N:
+        printBoard(board)
         return True
     res = False
-    for i in range(number):
-
-        if (isSafe(board, i, col, number)):
-
-            # Place this queen in board[i][col]
+    for i in range(N):
+        if valid_pos(board, i, col):
             board[i][col] = 1
+            res = Solver(board, col + 1) or res
 
-            # Make result true if any placement
-            # is possible
-            res = solveNQUtil(board, col + 1, number) or res
-
-            board[i][col] = 0  # BACKTRACK
-
+            board[i][col] = 0
     return res
 
 
-def solve(number):
-    """ Find all the posibilities if exists
-    Args:
-        number - size of the board
+def n_queen():
+    """Solves the N queens problem.
+    Return: None
     """
-    board = [[0 for i in range(number)]for i in range(number)]
-
-    if not solveNQUtil(board, 0, number):
-        return False
-
-    return True
-
-
-def validate(args):
-    """ Validate the input data to verify if the size to
-        answer is posible
-    Args:
-        args - sys.argv
-    """
-    if (len(args) == 2):
-        # Validate data
-        try:
-            number = int(args[1])
-        except Exception:
-            print("N must be a number")
-            exit(1)
-        if number < 4:
-            print("N must be at least 4")
-            exit(1)
-        return number
-    else:
-        print("Usage: nqueens N")
-        exit(1)
+    board = []
+    for row in range(N):
+        board.append([0] * N)
+    if not Solver(board, 0):
+        print("Not Solution Found")
+        return
+    return
 
 
-if __name__ == "__main__":
-    """ Main method to execute the application
-    """
-
-    number = validate(sys.argv)
-    solve(number)
+n_queen()
