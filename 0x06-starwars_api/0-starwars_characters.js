77bd500f9199a1a77910a16e212021dd26527a1d
kennywindis <kennyphilip100@gmail.com>
2023-06-28 05:16:44 -0700

    sa

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const urlApi = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-const promisifiedRequest = function (options) {
-  return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      if (response) {
-        return resolve(response);
-      }
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(filmUrl, (error, response, body) => {
+  if (error) {
+    return;
+  }
+  const film = JSON.parse(body);
+  const urlToName = {};
+  film.characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
       if (error) {
-        return reject(error);
+        return;
+      }
+      const character = JSON.parse(body);
+      urlToName[character.url] = character.name;
+      if (Object.keys(urlToName).length === film.characters.length) {
+        film.characters.forEach(characterUrl =>
+          console.log(urlToName[characterUrl]));
       }
     });
   });
-};
-
-request(urlApi, function (error, response, body) {
-  if (error) {
-    console.error(error); // Print the error if one occurred
-    return;
-  }
-  const jsonRes = JSON.parse(body);
-  const charUrl = jsonRes.characters;
-  (async function () {
-    for (let i = 0; i < charUrl.length; i++) {
-      const options = {
-        url: charUrl[i],
-        method: 'GET'
-      };
-
-      const response = await promisifiedRequest(options);
-
-      console.log(JSON.parse(response.body).name);
-    }
-  })();
 });
